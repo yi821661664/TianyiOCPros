1e03874426d1bed72584e0902e91e310ebc99311
易召强 <yizhaoqiang@tinman.cn>
2022-11-18 10:32:21 +0800

    索引问题

--- Pods/Target Support Files/TianyiAVManager/TianyiAVManager-umbrella.h
+++ Pods/Target Support Files/TianyiAVManager/TianyiAVManager-umbrella.h
@@ -14,6 +14,7 @@
 #import "TYAudioQueuePlayer.h"
 #import "TYAudioSystemPlayer.h"
 #import "TYAudioUnitPlayer.h"
+#import "TYAudioUnitReader.h"
 #import "TYAudioQueueRecorder.h"
 #import "TYAudioRecorder.h"
 #import "TYAudioSession.h"
